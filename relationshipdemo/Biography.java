648b231b5549eaf2be09202f17c506cd34c8997e
Raheem Abolfathzadeh <raheemabol.se@gmail.com>
2024-04-29 13:13:25 -0400

    Add files via upload

---
+++ relationshipdemo/Biography.java
@@ -0,0 +1,30 @@
+package relationshipdemo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Biography {
+
+    @Id
+    private int id;
+
+    private String bio;
+
+    public Biography(String bio) {
+        this.bio = bio;
+    }
+
+    public Biography(int id, String bio) {
+        this.id = id;
+        this.bio = bio;
+    }
+}
